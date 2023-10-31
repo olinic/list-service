79f8b37cf38b9a566b7b68e7ff340830b9e59cf6
Oliver <oliver.nic012@gmail.com>
2023-10-30 22:00:26 -0500

    Test fetching list items.

--- service/list_api/tests/test_list_items_api.py
+++ service/list_api/tests/test_list_items_api.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
+from service.list_api.models import ListItem
 
 class TestApi(TestCase):
    @classmethod
    def setUpTestData(cls):
-      pass
+      ListItem.objects.create(position=1, name="check")
 
    def setUp(self):
       pass
 
-   def test_this_is_false(self):
-      self.assertFalse(False)
+   def test_fetch_list_items(self):
+      response = self.client.get("/list-items/")
+      self.assertEqual(200, response.status_code)
